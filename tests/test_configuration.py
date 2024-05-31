4c848c0f73ab7e74c9c729af5d8047c32c1cd8c8
patrickpa <patrickabrahao.pa@gmail.com>
2024-05-31 14:20:18 +0200

    Update Configuration tests to use constants for file paths
    Signed-off-by: patrickpa <patrickabrahao.pa@gmail.com>

--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -1,17 +1,18 @@
+import os
 import pytest
 from qc_baselib.models import config, report
 from qc_baselib.configuration import Configuration
 
 
-@pytest.fixture
-def demo_config() -> str:
-    return "tests/data/DemoCheckerBundle_config.xml"
+DEMO_CONFIG_PATH = "tests/data/DemoCheckerBundle_config.xml"
+EXAMPLE_OUTPUT_CONFIG_PATH = "tests/data/ConfigTestOuput.xml"
+TEST_CONFIG_OUTPUT_PATH = "tests/ConfigTestOutput.xml"
 
 
 @pytest.fixture
-def loaded_config(demo_config: str) -> Configuration:
+def loaded_config() -> Configuration:
     config = Configuration()
-    config.load_from_file(demo_config)
+    config.load_from_file(DEMO_CONFIG_PATH)
     return config
 
 
@@ -113,3 +114,37 @@ def test_set_checker_param() -> None:
         config.get_checker_param("TestCheckerBundle", "TestChecker", "testCbParamFloat")
         == 2.0
     )
+
+
+def test_config_write() -> None:
+    config = Configuration()
+
+    config.set_config_param("testConfigParamStr", "testValue")
+    config.set_config_param("testConfigParamInt", 1)
+    config.set_config_param("testConfigParamFloat", 2.0)
+
+    config.register_checker_bundle("TestCheckerBundle")
+    config.register_checker_to_bundle(
+        "TestCheckerBundle", "TestChecker", min_level=1, max_level=3
+    )
+
+    config.set_checker_param(
+        "TestCheckerBundle", "TestChecker", "testCbParamStr", "testValue"
+    )
+    config.set_checker_param("TestCheckerBundle", "TestChecker", "testCbParamInt", 1)
+    config.set_checker_param(
+        "TestCheckerBundle", "TestChecker", "testCbParamFloat", 2.0
+    )
+
+    config.write_to_file(TEST_CONFIG_OUTPUT_PATH)
+
+    example_xml_text = ""
+    output_xml_text = ""
+    with open(EXAMPLE_OUTPUT_CONFIG_PATH, "rb") as config_xml_file:
+        example_xml_text = config_xml_file.read()
+    with open(TEST_CONFIG_OUTPUT_PATH, "rb") as config_xml_file:
+        output_xml_text = config_xml_file.read()
+
+    assert output_xml_text == example_xml_text
+
+    os.remove(TEST_CONFIG_OUTPUT_PATH)
